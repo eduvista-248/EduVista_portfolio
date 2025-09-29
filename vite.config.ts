2905d5ff0c730facf9bf95f35d1753086ebf6f1a
Bharathm7 <bharathmahesh69@gmail.com>
2025-09-29 13:18:50 +0530

    VITE changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/eduvista_portfolio",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
